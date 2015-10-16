526087d4bec829251d8fba7cb829d9d9337237c4
ValdemarViking <pechonko@gmail.com>
2015-10-16 18:50:15 +0300

    какие-то изменения

--- Lesson.14/src/Main.java
+++ Lesson.14/src/Main.java
@@ -4,6 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
+		Main main = new Main();
+		MyThread potok1 = new MyThread();
+		
 	}
 
 }
--- Lesson.14/src/Potok1.java
+++ Lesson.14/src/Potok1.java
@@ -1,4 +1,19 @@
 
-public class Potok1 {
+public class Potok1 extends Thread{
+	
+//	public final 
+	
+	public void run()
+	{
+		System.out.println ("Start potok 1");	
+		
+		
+		for (int i=0; i<10; i++){
+			System.out.println (i);
+		}
+		
+		System.out.println ("Stop potok 1");
+	}
+	
 
 }
