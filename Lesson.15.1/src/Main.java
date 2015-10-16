7213c787e9e0a8111080c71c91933e0ba000196b
ValdemarViking <pechonko@gmail.com>
2015-10-16 21:04:05 +0300

    урок 15

---
+++ Lesson.15.1/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static Integer int1 = 120;
+	public static Integer int2 = 120;
+	public static int int3 = 130;
+	public static Integer int4 = 130;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if (int1==int2){
+			System.out.println("true");
+		} else {
+			System.out.println("false");
+		}
+
+		if (int3==int4){
+			System.out.println("true");
+		} else {
+			System.out.println("false");
+		}
+	}
+
+}
---
+++ Lesson.15/src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test extends Testsuper implements Runnable {
+
+	private int privateField;
+	public String publicField;
+	
+	public void run() {
+		
+	}
+	
+	private void privateMethod(){
+		
+	}
+	public void publicMethod(){
+		System.out.println("publicMethod");
+	}
+	
+}
