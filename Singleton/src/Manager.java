edbf1e04ba72f5f2d35df5f6adc12a99ff653ff4
ValdemarViking <pechonko@gmail.com>
2015-10-19 21:12:17 +0300

    урок 16

--- Singleton/src/Manager.java
+++ Singleton/src/Manager.java
@@ -1,4 +1,15 @@
 
 public class Manager {
+	private static Manager instanse = new Manager();
+	
+	private Manager () {}
+	
+	public static Manager getInstanse() {
+		return instanse;
+	}
+	
+	
+
+	
 
 }
